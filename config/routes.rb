f53b8699b0b007ed56c65a136b132be6ca1cfcb9
Joseph Wiel <joseph@comet.co>
2019-09-16 15:46:05 +0200

    handle pro subscription

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ Rails.application.routes.draw do
   get 'pages/home'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  post '/add_to_list', to: 'mailchimp#add_to_list'
 
 end
